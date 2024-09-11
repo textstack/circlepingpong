42d844a36092d72d8a9dc1472a82f8a60fa152f4
JudeTheDude5 <reynoldj26@up.edu>
2024-09-11 13:15:10 -0800

    Update pause_menu.gd

--- pause_menu.gd
+++ pause_menu.gd
@@ -25,6 +25,7 @@ func _on_quit_pressed() -> void:
 	get_tree().quit()
 	
 func _on_restart_pressed() -> void:
+	get_tree().paused = false
 	get_tree().reload_current_scene()
 	
 func _process(delta):
