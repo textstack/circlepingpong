f6e01f115841dc220b19a175734d9c5960418222
phamc25 <phamc25@up.edu>
2024-09-30 11:47:03 -0700

    commented out nux

--- gamemodes/nux mode.gd
+++ gamemodes/nux mode.gd
@@ -1,33 +1,33 @@
 extends EnduranceGamemode
 class_name NuxMode
 
-## Called when the node enters the scene tree for the first time.
-#func _ready() -> void:
-	#speedMult = 1
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	speedMult = 1
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+#func onBallHit(ball, _collision) -> void:
+	#points += 1
+	#ballPoints += 1
 #
-## Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-	#pass
-	
-func onBallHit(ball, _collision) -> void:
-	points += 1
-	ballPoints += 1
-
-	# Slow down the ball by reducing its velocity by 3% (for example)
-	ball.velocity = ball.velocity * 0.97
-	
-	if ballPoints >= determinePointsNeeded():
-		mainScene.createBall()
-		
-	if hits > hitsForPowerup and powerUpCount() < 1:
-		spawnPowerup()
-		hits = 0
-		hitsForPowerup += hitsIncrease
-	else:
-		hits += 1
-
-	if x2power:
-		points += 1
-	
+	## Slow down the ball by reducing its velocity by 3% (for example)
+	#ball.velocity = ball.velocity * 0.97
+	#
+	#if ballPoints >= determinePointsNeeded():
+		#mainScene.createBall()
+		#
+	#if hits > hitsForPowerup and powerUpCount() < 1:
+		#spawnPowerup()
+		#hits = 0
+		#hitsForPowerup += hitsIncrease
+	#else:
+		#hits += 1
+#
+	#if x2power:
+		#points += 1
+	#
 func getName() -> String:
 	return "Nux Mode"
