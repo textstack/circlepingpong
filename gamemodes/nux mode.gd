e35d217527eab83fbd7f2d5ec8c4d9cdd492c8d7
phamc25 <phamc25@up.edu>
2024-09-30 10:50:14 -0700

    nux mode

---
+++ gamemodes/nux mode.gd
@@ -0,0 +1,13 @@
+extends EnduranceGamemode
+class_name NuxMode
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+func getName() -> String:
+	return "Nux Mode"
